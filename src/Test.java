205200c2653d77eef1860e0cc45859c544fa507c
DESKTOP-GT8L6OA\caihu <caihua2600@gmail.com>
2017-05-08 10:47:26 +1000

    hello

--- src/Test.java
+++ src/Test.java
@@ -3,5 +3,6 @@ public class Test {
      public static void main(String[] args){
     	 System.out.println("hello git");
     	 System.out.println("hello team.");
+    	 System.out.println("hello allen.");
      }
 }
