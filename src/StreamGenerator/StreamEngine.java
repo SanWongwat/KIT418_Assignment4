8bb322cdd0fbbfebf4d25b576e7bc716c51e10dd
DESKTOP-GT8L6OA\caihu <caihua2600@gmail.com>
2017-05-24 20:20:29 +1000

    g

--- src/StreamGenerator/StreamEngine.java
+++ src/StreamGenerator/StreamEngine.java
@@ -1,10 +1,42 @@
 package StreamGenerator;
 
-public class StreamEngine {
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
+public class StreamEngine {
+	public static  String Server_HOSTNAME;
+    public static  int SERVER_PORT;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//1 read a file line by line 
+		// output them to a socket.
+		PrintWriter out=null;
+		    try{
+			FileReader fr=new FileReader("C:\\Users\\caihu\\OneDrive\\Documents\\KIT418_Assignment4\\src\\StreamGenerator\\string.txt");
+			char[] cbuf=new char[32];
+			int hasRead=0;
+			Socket socket=new Socket(Server_HOSTNAME,SERVER_PORT);
+			out=new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+			while((hasRead=fr.read(cbuf))>0){
+				System.out.println(new String(cbuf,0,hasRead));
+				Thread.sleep(1000);
+				out.println(new String(cbuf,0,hasRead));
+				out.flush();
+			}
+		    }catch(FileNotFoundException e){
+		    	e.printStackTrace();
+		    }catch(IOException e){
+		    	e.printStackTrace();
+		    } catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		   
 	}
 
 }
