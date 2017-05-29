91066b5d7e9b563bbe41d074a65d772cb5a65d1e
DESKTOP-GT8L6OA\caihu <caihua2600@gmail.com>
2017-05-29 20:04:05 +1000

    multi threads for StreamGenerator

---
+++ src/StreamGenerator/ClientInfoThread.java
@@ -0,0 +1,38 @@
+package StreamGenerator;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.URL;
+
+public class ClientInfoThread implements Runnable{
+	Socket s;
+	BufferedReader br=null;
+	public String content;
+	public ClientInfoThread(Socket socket) throws IOException{
+		this.s=socket;
+		
+	}
+	public void run(){
+		try{
+			URL path = StreamEngine.class.getResource("intimeof.txt");
+			FileReader fr = new FileReader(path.getFile());
+			char[] cbuf = new char[32];
+			int hasRead = 0;
+			DataOutputStream _dos = new DataOutputStream(s.getOutputStream());
+			System.out.println("Streaming content...");
+			while ((hasRead = fr.read(cbuf)) > 0) {
+				_dos.writeUTF(new String(cbuf, 0, hasRead));
+				//System.out.println(new String(cbuf, 0, hasRead));
+				Thread.sleep(2000);
+			}
+		}catch(IOException | InterruptedException e){
+			e.printStackTrace();
+		}
+	}
+
+}
--- src/StreamGenerator/StreamEngine.java
+++ src/StreamGenerator/StreamEngine.java
@@ -6,40 +6,39 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.URL;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 
 public class StreamEngine {
 	public static String Server_HOSTNAME = "localhost";
 	public static int SERVER_PORT = 1254;
+	public static ArrayList<Socket> socketList=new ArrayList<Socket>();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// 1 read a file line by line
 		// output them to a socket.
-
+		ServerSocket serversocket=null;
 		try {
-			URL path = StreamEngine.class.getResource("intimeof.txt");
-			FileReader fr = new FileReader(path.getFile());
-			char[] cbuf = new char[32];
-			int hasRead = 0;
-			Socket socket = new Socket(Server_HOSTNAME, SERVER_PORT);
-			DataOutputStream _dos = new DataOutputStream(socket.getOutputStream());
-			System.out.println("Streaming content...");
-			while ((hasRead = fr.read(cbuf)) > 0) {
-				_dos.writeUTF(new String(cbuf, 0, hasRead));
-				//System.out.println(new String(cbuf, 0, hasRead));
-				Thread.sleep(2000);
+			
+			serversocket=new ServerSocket(SERVER_PORT);
+			while(true){
+				Socket ss=serversocket.accept();
+				socketList.add(ss);
+				new Thread(new ClientInfoThread(ss)).start();
 			}
+			
+			
+			
+			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		} 
 
 	}
 
