77e37df2d7326f8a605bbb9c7b7245a29d5f3583
DESKTOP-GT8L6OA\caihu <caihua2600@gmail.com>
2017-05-31 18:45:05 +1000

    nearly finished algorithm part of word count.

--- src/WordCount/WordCountEngine.java
+++ src/WordCount/WordCountEngine.java
@@ -6,6 +6,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 public class WordCountEngine {
@@ -20,29 +21,44 @@ public class WordCountEngine {
 		// TODO Auto-generated method stub
 		//ServerSocket ss;
 		int k=Integer.parseInt(args[1]);
+		k=3;
 		try{
 			//ss = new ServerSocket(PORT_NUMBER);
 			//System.out.println("WordCountEngine start at port 1254");
 			//start listen to the stream generator
 			Socket socket=new Socket("localhost",PORT_NUMBER);
 			br=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			int number=0;
 			while(br.readLine()!=null){
 				String content=br.readLine();
 				System.out.println(content);
 				// for each content , done the calculation.
 				//if the string already exists, done the following
+				
 				if(resultSet.keySet().contains(content))
 			     {
 			          Integer count = resultSet.get(content) + 1;
 			          resultSet.put(content, count);
 			     }
-				//if the string does not exist in the current set, 
-			     else{
+				
+			     else if(number<k){
 			          resultSet.put(content, 1);
+			          number=number+1;
 
+			      }else{
+			    	  Iterator<String> it=resultSet.keySet().iterator();
+			    	  while(it.hasNext()){
+			    		  Integer count=resultSet.get(it.next())-1;
+			    		  if(count>0){
+			    		  resultSet.put(it.next(), count);
+			    		  }else{
+			    			  resultSet.remove(it.next());
+			    		  }
+			    	  }
 			      }
 			}
-			resultSet=MapUtil.sortByValue(resultSet);
+			
+			
 			
 			
 			
